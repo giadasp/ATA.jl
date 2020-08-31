9894271988eeeb760ac0d62b49939aa51e0f3b86
giadasp <giada.spaccapanico@gmail.com>
2020-08-31 14:53:30 +0200

    better comments

--- examples/parallelExample.jl
+++ examples/parallelExample.jl
@@ -59,44 +59,60 @@ println(add_obj_fun!(ATAmodel)[2])
 
 #Assembly settings
 
-#Set the solver, "siman" for simulated annealing, "jumpATA" for MILP solver.
+# Set the solver, "siman" for simulated annealing, "jumpATA" for MILP solver.
 solver = "siman"
 
-#SIMAN (Suggested for Large scale ATA):
+# SIMAN (Suggested for Large scale ATA):
 
-#Stopping criteria:
-#Time limit in seconds
-max_time = 100
-#Minimum convergence, stop when, after max_conv rounds no improvements have been found.
-#Set to minimum for shallow analysis, increase it for deep analysis.
-conv_max = 5 # 2 <= conv_max <= Inf
+start_temp = 0.0001
+# Default: `0.1`. Values:  `[0, Inf]`. 
+# Starting temperature, set to minimum for short journeys (if 0 worse solutions will never be accepted).
 
-#Starting temperature, set to minimum for short journeys (if 0 worse solutions will never be accepted)
-start_temp = 0.0001 #0 <= start_temp <= Inf
-#Deacresing geometric factor
-geom_temp  = 0.1  #0 <= start_temp <= Inf
+geom_temp = 0.1
+# Default: `0.1`. Values:  `[0, Inf)`.
+# Decreasing geometric factor.
 
-#Number of fill up phases, usually 1 is sufficient, if start_temp is high can be setted high.
-# if a starting_design is supplied, can be setted to 0.
-n_fill = 1
-#Set deep analysis of neighbourhoods, set both to 1 for a shallow analysis.
-n_item_sample = maximum([ATAmodel.Cosntraints[t].length_max for t=1:ATAmodel.settings.T])
-# 1 <= n_item_sample <= Inf
+n_item_sample = Inf
+# Default: 1. Values: `[1, Inf]`. 
+# Number of items to alter. Set to minimum for a shallow analysis, 
+# set to maximum for a deep analysis of the neighbourhoods.
+
 n_test_sample = ATAmodel.settings.T
-# 1 <= n_test_sample <= Inf
+# Default: 1. Values: `[1, Inf]`. 
+# Number of tests to alter. Set to minimum for a shallow analysis, set to maximum for a deep analysis of the neighbourhoods.
 
-#Number of Feasibility neighbourhoods to explore, set to the minimum if the model is small or not highly constrained
-feas_nh = 1 # 0 <= feas_nh <= Inf
-#Number of Optimality neighbourhoods to explore, set to the minimum if the model is highly constrained
-opt_nh = 200 # 0 <= feas_nh <= Inf
-#Optimality/feasibility balancer, if 0 only feasibility of solution is analysed.
-#Viceversa, if 1, only optimality is considered (uncontrained model)
-opt_feas = 0.9 # 0 <= opt_feas <= 1
+opt_feas = 0.9
+# Default: 0.0. Values: `[0, Inf)`. 
+# Optimality/feasibility balancer, if 0 only feasibility of solution is analysed. Viceversa, if 1, only optimality is considered (uncontrained model). All the other values are accepted but produce uninterpretable results.
 
-#Verbosity, in the console '+' stands for improvement, '_' for accepting worse solution.
-verbosity = 2 # 1 = minimal, 2 = detailed.
+n_fill = 1
+# Default: 1. Values: `[0, Inf)`.
+# Number of fill-up phases, usually 1 is sufficient, if start_temp is high it can be higher. 
+# If a starting_design is supplied, it should be set to 0.
+ 
+verbosity = 2
+# Default: 2. Values: `1` (minimal), `2` (detailed).
+# Verbosity level. In the console '+' stands for improvement, '_' for accepting worse solution.
+# The dots are the fill-up improvement steps.
 
-#MILP (Look at test.jl for use MILP solvers)
+#! Termination criteria: 
+
+max_time = 10.0
+# Default: `1000.0`. Values: `[0, Inf)`.
+# Time limit in seconds.
+
+max_conv = 5
+# Default: `2`. Values: `[1, Inf)`. 
+# Maximum convergence, stop when, after max_conv rounds no improvements have been found. 
+# Set to minimum for shallow analysis, increase it for deep analysis of neighbourhoods.
+
+feas_nh = 1
+# Default: `0`. Values: `[1, Inf)`. 
+# Maximum number of Feasibility neighbourhoods to explore, set to the minimum if the model is small or not highly constrained.
+
+opt_nh = Inf
+# Default: `5`. Values: `[1, Inf)`. 
+# Maximum number of Optimality neighbourhoods to explore, set to the minimum if the model is highly constrained.
 
 #9. assemble
 assemble!(ATAmodel;
@@ -107,7 +123,7 @@ assemble!(ATAmodel;
     n_item_sample = n_item_sample,
     n_test_sample = n_test_sample,
     verbosity = verbosity,
-    conv_max = conv_max,
+    max_conv = max_conv,
     opt_feas = opt_feas,
     n_fill = n_fill,
     feas_nh = feas_nh,
