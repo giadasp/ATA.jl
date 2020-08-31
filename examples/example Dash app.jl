7074be55598b04de6bfdf77b86b6d9007e90f898
giadasp <giada.spaccapanico@gmail.com>
2020-08-31 23:10:09 +0200

    add dash example

---
+++ examples/example Dash app.jl
@@ -0,0 +1,16 @@
+# cd("folder in which the package is saved")
+# using Pkg
+# Pkg.activate(".")  # required
+# Pkg.instantiate()
+# cd("where your input files are")
+using ATA
+using Dash
+using DashCoreComponents
+using DashHtmlComponents
+
+# Before running the app, if you want to use a MILP solver, remember to load it
+# (ex: using Cbc; run_app!()).
+run_app!()
+# Navigate with the browser to localhost:8080
+
+
