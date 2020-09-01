fb9d25e87a864907c601f5ecc282035b783e55d2
giadasp <giada.spaccapanico@gmail.com>
2020-09-01 11:28:12 +0200

    change devurl

--- docs/make.jl
+++ docs/make.jl
@@ -18,11 +18,5 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/giadasp/ATA.jl.git"
+    repo = "giadasp@github.com/giadasp/ATA.jl.git"
 )
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#= deploydocs(
-    repo = "<repository url>"
-) =#
