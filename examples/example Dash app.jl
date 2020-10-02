0349ea0567b82ba2d17041dcb2fec27205a6756a
giadasp <giada.spaccapanico@gmail.com>
2020-10-03 01:49:50 +0200

    moved dash app to ATADash

--- examples/example Dash app.jl
+++ examples/example Dash app.jl
@@ -4,11 +4,9 @@
 # Pkg.instantiate()
 # cd("where your input files are")
 using ATA
-using Dash
-using DashCoreComponents
-using DashHtmlComponents
+using ATADash
 
 # Before running the app, if you want to use a MILP solver, remember to load it
-# (ex: using Cbc; run_app!()).
-run_app!()
+# (ex: using Cbc; run_app()).
+run_app()
 # Navigate with the browser to localhost:8080
