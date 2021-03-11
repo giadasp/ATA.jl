3ad01d79613dd151f3110f5646e9d46cc78b5803
giadasp <giada.spaccapanico@gmail.com>
2021-03-11 18:57:10 +0100

    fix no objective obj name

--- src/structs/models/no_obj_model.jl
+++ src/structs/models/no_obj_model.jl
@@ -1,7 +1,9 @@
 mutable struct NoObjModel <: AbstractModel
     settings::Settings
     constraints::Vector{Constraint}
+    obj::NoObjective
     output::Output
-    NoObjModel(settings, constraints, output) = new(settings, constraints, output)
-    NoObjModel() = new(Settings(), Constraint[], Output())
+
+    NoObjModel(settings, constraints, obj, output) = new(settings, constraints, obj, output)
+    NoObjModel() = new(Settings(), Constraint[], NoObjective(), Output())
 end
