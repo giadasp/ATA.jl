f4368922939a7ebaa9eebcdca3ae4d2668e9c571
giadasp <giadasp@users.noreply.github.com>
2021-05-24 14:43:38 +0000

    deploy: 8ec4227c418784521e8d14623626e072a348fc79

---
+++ docs/make_release.jl
@@ -0,0 +1,15 @@
+using Documenter
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+using ATA
+
+makedocs(
+    sitename = "ATA",
+    format = Documenter.HTML(),
+    modules = [ATA],
+    doctest = true,
+    pages = ["index.md", "model_build.md", "opt.md", "compact.md", "print_and_plot.md", "structs.md", "utils.md", "examples.md"],
+)
+
+deploydocs(repo = "github.com/giadasp/ATA.jl.git", devurl = "docs", devbranch = "release")
